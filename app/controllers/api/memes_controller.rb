6712d1eda265416438e839678b3ffdacf31b733f
James Yeates <jimibue@gmail.com>
2021-10-06 11:33:56 -0600

    demo

--- app/controllers/api/memes_controller.rb
+++ app/controllers/api/memes_controller.rb
@@ -1,6 +1,25 @@
 class Api::MemesController < ApplicationController
   def create
-    puts "-------HERE--------"
-    puts params
+    file = params[:file]
+    ## first we want to save the image to cloudinary -> (url of the image)
+    if file
+      begin
+        cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true, resource_type: :auto)
+        ## successfully saved to cloudinary
+        ## once successfull we want to save to our database
+        # cloud_image["secure_url"] this is the url for our image
+        meme = Meme.new(text: params[:text], image: cloud_image["secure_url"])
+        if meme.save
+          # did save to cloudianry and db
+          render json: meme
+        else
+          # did save to cloudianry but not to db
+          render json: { errors: meme.errors }, status: 422
+        end
+      rescue => err
+        # did not save to cloudinary
+        render json: { errors: err }, status: 422
+      end
+    end
   end
 end
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,52 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Image } from "semantic-ui-react";
+
+// Import React FilePond
+import { FilePond, registerPlugin } from "react-filepond";
+
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 export default function Home() {
   const [text, setText] = useState("");
+  const [files, setFiles] = useState([]);
+  const [url, setUrl] = useState(
+    "https://react.semantic-ui.com/images/wireframe/image.png"
+  );
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(text);
     try {
-      let res = await axios.post("/api/memes", { text });
+      // send image stuff in files state
+      let data = new FormData();
+      data.append("file", files[0].file);
+      data.append("text", text);
+      let res = await axios.post("/api/memes", data);
+      console.log(res);
+      setUrl(res.data.image);
     } catch (err) {
+      console.log(err);
+      console.log(err.response);
       alert("err in post meme occured");
     }
+  };
+
+  const fileChanged = (fileItems) => {
+    if (fileItems[0]) {
+      console.log(fileItems[0].file);
+    }
+    setFiles(fileItems);
   };
   return (
     <div>
@@ -23,8 +59,18 @@ export default function Home() {
           onChange={(e) => setText(e.target.value)}
         />
         <br />
+        <FilePond
+          files={files}
+          allowMultiple={false}
+          // onupdatefiles={setFiles}
+          onupdatefiles={fileChanged}
+          labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+        />
+        <br />
         <button type="submit">add</button>
       </form>
+
+      <Image src={url} size="large" wrapped />
     </div>
   );
 }
